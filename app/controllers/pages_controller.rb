1309b3c5e20c5fc90c4a0eb231c15d86c0c2f3c2
Sabr_System <ilyyasa03@gmail.com>
2024-02-28 07:50:14 +0700

    user login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:users], unless: :admin_signed_in?
+  before_action :authenticate_admin!, only: [:admin]
   def home
   end
 
